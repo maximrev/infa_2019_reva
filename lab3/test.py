82dc8c145c74615e867a8e7b90e00849eeb2e0b8
Reva Maxim <reva.ma@phystech.edu>
2019-09-19 15:05:41 +0300

    test

---
+++ lab3/test.py
@@ -0,0 +1,18 @@
+from graph import *
+
+brushColor("yellow")
+circle(250,250,150)
+
+brushColor("red")
+circle(190,215,30)
+circle(310,215,20)
+
+brushColor("black")
+circle(190,215,10)
+circle(310,215,10)
+
+rectangle(190,350,310,375)
+
+polygon([(100,110),(230,210),(240,200),(110,100)])
+polygon([(275,210),(400,160),(400,150),(275,200)])
+run()
